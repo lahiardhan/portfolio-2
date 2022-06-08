96a5e62ad228c4f3122dff6ef3ca4b1b463f9f6b
Muhammad Lahia Ardhan <lahia.ardhanm@gmail.com>
2022-06-08 20:46:07 +0700

    first push

--- src/data/scrollRevealConfig.js
+++ src/data/scrollRevealConfig.js
@@ -58,6 +58,20 @@ export const targetElements = [
     },
   },
   {
+    element: ".ability-wrapper__text",
+    animation: {
+      delay: 500,
+      origin: window.innerWidth > 768 ? "left" : "bottom",
+    },
+  },
+  {
+    element: ".ability-wrapper__image",
+    animation: {
+      delay: 1000,
+      origin: window.innerWidth > 768 ? "right" : "bottom",
+    },
+  },
+  {
     element: ".contact-wrapper",
     animation: {
       delay: 800,
